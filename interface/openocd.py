448a8f74f64176c6365ed0ea920955cf1e3c143d
Andreas Lindner <andili@kth.se>
2019-08-24 01:13:44 +0200

    add support for lpc11c24

--- interface/openocd.py
+++ interface/openocd.py
@@ -24,11 +24,9 @@ board_id  = (args.box_name, args.board_name)
 # load config
 config = boxconfig.BoxConfig()
 
-interface_cfg_dict = {"RPi3" : "../config/openocd/interface/minimodule.cfg", \
-                      "RPi2" : "../config/openocd/interface/minimodule.cfg"}
-
-target_cfg_dict = {"RPi3" : "../config/openocd/target/rpi3.cfg", \
-                   "RPi2" : "../config/openocd/target/rpi2.cfg" }
+target_cfg_dict = {"RPi3"     : config.get_boxpath("config/openocd/target/rpi3.cfg"), \
+                   "RPi2"     : config.get_boxpath("config/openocd/target/rpi2.cfg"), \
+                   "LPC11C24" : "target/lpc11xx.cfg"}
 
 # find jtag serial number
 try:
@@ -38,8 +36,15 @@ except:
 	exit(-2)
 
 _JTAG_MINIMOD_SERNUM = "jtag_minimodule_serialnumber"
+_JTAG_CMSISDAP_SERNUM = "jtag_cmsisdap_serialnumber"
 if _JTAG_MINIMOD_SERNUM in board_params:
 	jtag_ftdi_serial = board_params[_JTAG_MINIMOD_SERNUM]
+	interface_cfg    = config.get_boxpath("config/openocd/interface/minimodule.cfg")
+	command_interface_sel = ["-c", "ftdi_serial %s" % jtag_ftdi_serial]
+elif _JTAG_CMSISDAP_SERNUM in board_params:
+	jtag_cmsis_serial = board_params[_JTAG_CMSISDAP_SERNUM]
+	interface_cfg    = "interface/cmsis-dap.cfg"
+	command_interface_sel = ["-c", "cmsis_dap_serial %s" % jtag_cmsis_serial]
 else:
 	assert False
 
@@ -50,23 +55,22 @@ if board_idx < 0 or 99 < board_idx:
 
 (_, ((oocd_gdb_port_base,oocd_gdb_port_len), oocd_telnet_port, oocd_tcl_port)) = boxportdistrib.get_ports_box_server_board(board_idx)
 
-print(jtag_ftdi_serial)
+print(interface_cfg)
+print(command_interface_sel)
 print(board_id)
 print(board_idx)
 print(((oocd_gdb_port_base,oocd_gdb_port_len), oocd_telnet_port, oocd_tcl_port))
 print(20 * "=")
 
-os.chdir(config.get_boxpath("tools"))
+os.chdir(config.get_boxpath("tools/openocd/tcl"))
 
 board_type = board_params["Type"]
-interface_cfg         = interface_cfg_dict[board_type]
-command_interface_sel = ["-c", "ftdi_serial %s" % jtag_ftdi_serial]
 commands_ports        = ["-c", "tcl_port %d"    % (oocd_tcl_port), \
 			 "-c", "gdb_port %d"    % (oocd_gdb_port_base), \
 			 "-c", "telnet_port %d" % (oocd_telnet_port)]
 target_cfg            = target_cfg_dict[board_type]
 
-cmd_list = ["openocd/src/openocd", "-f", interface_cfg] + command_interface_sel + commands_ports + ["-f", target_cfg]
+cmd_list = ["../src/openocd", "-f", interface_cfg] + command_interface_sel + commands_ports + ["-f", target_cfg]
 
 toolwrapper.SimpleWrapper(cmd_list, timeout=5)
 
