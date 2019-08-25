d660d81c9f66f6198b536051ca7ca2bdd86724f5
Andreas Lindner <andili@kth.se>
2019-08-25 03:26:29 +0200

    fix

--- tools/startup/startup.sh
+++ tools/startup/startup.sh
@@ -4,11 +4,14 @@
 THIS_DIR=$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")
 BASE_DIR=$(readlink -f "${THIS_DIR}/../..")
 
+# log directory
+LOG_DIR=${BASE_DIR}/logs
 
 # setup firewall (iptables)
 ${BASE_DIR}/tools/startup/firewall.sh
 
 # start the box_server and fork it off
-/usr/bin/python3 -u ${BASE_DIR}/tools/box_server/server.py >${BASE_DIR}/logs/box_server.log 2>&1 &
+mkdir -p ${LOG_DIR}
+/usr/bin/python3 -u ${BASE_DIR}/tools/box_server/server.py >${LOG_DIR}/box_server.log 2>&1 &
 
 
