4b0545363f28dc0e6ff2d69b0c657c9c3298ee81
Andreas Lindner <andili@kth.se>
2021-05-13 02:41:11 +0200

    Fix

--- tools/startup/startup.sh
+++ tools/startup/startup.sh
@@ -11,7 +11,6 @@ LOG_DIR=${BASE_DIR}/logs
 ${BASE_DIR}/tools/startup/firewall.sh
 
 # start the box_server and fork it off
-sleep 10
 datetime=$(date "+%Y-%m-%d_%H-%M-%S")
 mkdir -p ${LOG_DIR}
 /usr/bin/python3 -u ${BASE_DIR}/tools/box_server/server.py >"${LOG_DIR}/box_server_${datetime}.log" 2>&1 &
